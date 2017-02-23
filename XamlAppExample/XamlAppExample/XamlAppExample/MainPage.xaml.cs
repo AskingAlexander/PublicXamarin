9eab5b251d0e91fa6b415b7be8537c01b00d17a7
Xander96 <petrescu_alexandru96@yahoo.com>
2017-02-23 15:34:16 +0200

    Added Base Project

---
+++ XamlAppExample/XamlAppExample/XamlAppExample/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamlAppExample
+{
+	public partial class MainPage : ContentPage
+	{
+        static string[] possibleHexes = {"30BCED", "FFFAFF", "FC5130", "102B3F"};
+
+		public MainPage()
+		{
+            InitializeComponent();
+            this.Content = new StackLayout
+            {
+                Children =
+                {
+                   IAmText,
+                   clickMe
+                }
+            };
+
+            clickMe.Clicked += IWasClicked;
+		}
+
+        private void IWasClicked(object sender, EventArgs e)
+        {
+            IAmText.BackgroundColor = Color.FromHex(GiveRandomColor());
+        }
+
+        private string GiveRandomColor()
+        {
+            return possibleHexes[GiveRandomNumber()];
+        }
+
+        private int GiveRandomNumber()
+        {
+            return ((new Random()).Next(0, possibleHexes.Length));
+        }
+    }
+}
